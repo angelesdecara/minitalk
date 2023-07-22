cb8f50cdca7f351a2f9bd892880bfb54afec26e2
Angeles Rodriguez De Cara <angrodri@c1r10s6.42madrid.com>
2023-07-22 20:28:07 +0200

    first commit

---
+++ test_sigaction.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_sigaction.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angrodri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/22 18:54:57 by angrodri          #+#    #+#             */
+/*   Updated: 2023/07/22 19:05:35 by angrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <signal.h>
+#include <stdio.h>
+#include <strings.h>
+
+void	sigint_handler(int signal)
+{
+	if (signal == SIGINT)
+		printf("\nIntercepted SIGINT!\n");
+}
+
+void	set_signal_action(void)
+{
+	struct sigaction	act; //sigaction structure 
+
+	bzero(&act, sizeof(act)); /*set all structure bits to 0 to avoid errors due to uninitialised vars*/
+	act.sa_handler = &sigint_handler; /*signal handler set to default action*/
+	sigaction(SIGINT, &act, NULL); /* apply the action in the structure to SIGINT signal, ctrl-c*/
+}
+
+int	main(void)
+{
+	set_signal_action();
+	while (1)
+		continue;
+	return (0);
+}
